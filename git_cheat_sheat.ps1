8531dffabcbe725cc02155ffdb02bfb0573f6b03
Razvan Goga <razvan.goga@gmail.com>
2019-07-11 11:06:08 +0200

    Update git_cheat_sheat.ps1

--- git_cheat_sheat.ps1
+++ git_cheat_sheat.ps1
@@ -15,7 +15,7 @@ git push origin --tags
 #git flow
 #init git flow with it's default options
 git flow init -d
-#see gitflows current confgigs - can be manuall seen in .git/config file - all configs for the current repo are keys there
+#see gitflows current confgigs - can be manually seen in .git/config file - all configs for the current repo are keys there
 git flow config
 
 #start/finish a new git flow branch
