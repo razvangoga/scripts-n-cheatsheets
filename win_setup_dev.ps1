184eb3429deecdb613d57dd691cdbce23b1c25b1
Razvan Goga <razvan.goga@gmail.com>
2019-08-30 12:18:32 +0200

    extended dev script

--- win_setup_dev.ps1
+++ win_setup_dev.ps1
@@ -6,6 +6,7 @@ choco install azure-cli -y
 choco install azure-data-studio -y
 choco install chromium -y
 choco install docker-desktop -y
+choco install drawio -y
 choco install foxitreader -y
 choco install git -y
 choco install golang -y
@@ -13,18 +14,22 @@ choco install hg -y
 choco install linqpad -y
 choco install microsoftazurestorageexplorer -y
 choco install nodejs -y
+choco install nswagstudio -y
 choco install poshgit -y
 choco install postman -y
 choco install ServiceBusExplorer -y
 choco install slack -y
+choco install microsoft-windows-terminal -y
 choco install totalcommander -y
 choco install visualstudiocode -y
 choco install winmerge -y
 
 #configure vs code
 code --install-extension DotJoshJohnson.xml
+code --install-extension johnpapa.vscode-peacock
 code --install-extension ms-vscode.azurecli
 code --install-extension ms-vscode.csharp
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vscode.powershell
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension PeterJausovec.vscode-docker
@@ -32,3 +37,14 @@ code --install-extension PeterJausovec.vscode-docker
 #extra win features
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
+#wsl distros
+#https://docs.microsoft.com/en-us/windows/wsl/install-manual
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ubuntu.appx -UseBasicParsing
+Add-AppxPackage .\ubuntu.appx
+
+Invoke-WebRequest -Uri https://aka.ms/wsl-kali-linux -OutFile kali.appx -UseBasicParsing
+Add-AppxPackage .\kali.appx
+
+Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile debian.appx -UseBasicParsing
+Add-AppxPackage .\debian.appx
