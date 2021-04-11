ec5298a693eab57312d5b3a5282b8ba63e841451
Razvan Goga <razvan.goga@gmail.com>
2021-04-11 17:15:24 +0200

    added pwoershell core

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13,5 +13,15 @@ sudo apt install code
 #azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+# powershell core
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+
+
 #other tools
 sudo apt install git
