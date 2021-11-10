0ac56444ab566d2d614892c5f82e8dbc95c81758
Razvan Goga <razvan@avalnet.ro>
2021-11-10 12:50:07 +0100

    added msi afterburner

--- win_setup_gaming.ps1
+++ win_setup_gaming.ps1
@@ -5,5 +5,6 @@ choco install epicgameslauncher -y
 choco install geforce-experience -y
 choco install goggalaxy -y
 choco install logitechgaming -y
+choco install msiafterburner -y
 choco install origin -y
 choco install steam -y
