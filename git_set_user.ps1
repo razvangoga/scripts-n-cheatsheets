cdf10dafa5105b21774c49239178419cde9f6a5c
razvangoga <razvan.goga@gmail.com>
2023-03-30 11:06:07 +0200

    extra commands to run in each git repo of project folder

--- git_set_user.ps1
+++ git_set_user.ps1
@@ -14,43 +14,62 @@ $profilePath = Join-Path -Path $folder -ChildPath $profileFileName
 $credentialsConfigExist = Test-Path -Path $profilePath
 $credentialsGivenAsPrms = $userName -And $email 
 
-$userNameToSet = ''
-$emailToSet = ''
+
+$credentials = [PSCustomObject]@{
+    userName = ''
+    email    = ''
+    commands = @()
+}
+
 $hasGitRepos = $false
 
-if($credentialsConfigExist) {
+
+if ($credentialsConfigExist) {
     $credentials = (Get-Content $profilePath) | ConvertFrom-Json
-    $userNameToSet = $credentials.userName
-    $emailToSet = $credentials.email
-} elseif ($credentialsGivenAsPrms) {
-    $userNameToSet = $userName
-    $emailToSet = $email
-} else {
+}
+elseif ($credentialsGivenAsPrms) {
+    $credentials.userName = $userName
+    $credentials.email = $email
+}
+else {
     Write-Error "Give user credentials either via parameters or via an $profileFileName located in the target folder"
     Exit 1
 }
 
-Write-Host "Setting the userName and email for all repos to <$userNameToSet ($emailToSet)>"
+Clear-Host
+Write-Host "Setting the userName and email for all repos to <$($credentials.userName) ($($credentials.email))>"
 Write-Host ""
+
+$originalLocaltion = Get-Location
 
 Get-ChildItem -Directory -Path $folder | Foreach-Object {
     $repo = $_.FullName
     $gitFolder = Join-Path -Path $repo -ChildPath '.git'
     if (-not (Test-Path -Path $gitFolder)) {
         Write-Warning "$repo -> is not a git repo"
-    } else {
+    }
+    else {
         Write-Host "$repo -> setting user info"
         $hasGitRepos = $true
 
-        & git -C $repo config user.email $userNameToSet
-        & git -C $repo config user.name $emailToSet
+        & git -C $repo config user.email $credentials.userName
+        & git -C $repo config user.name $credentials.email
+
+        Set-Location $repo
+
+        foreach ($command in $credentials.commands)
+        {
+            Invoke-Expression $command
+        }
     }
 }
 
+Set-Location $originalLocaltion
 Write-Host ""
 
-if($hasGitRepos) {
+if ($hasGitRepos) {
     Write-Host "User updated in all git repos found"
-} else {
+}
+else {
     Write-Warning "No git repos found to update"
 }
