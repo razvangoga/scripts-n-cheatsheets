b6344ece69dcc4f25e2747e2455e208ad4c6cd5e
razvangoga <razvan.goga@gmail.com>
2024-07-08 15:58:47 +0200

    update codium install script

--- codium-extensions/codium.ps1
+++ codium-extensions/codium.ps1
@@ -6,13 +6,14 @@ codium --install-extension eamodio.gitlens
 codium --install-extension GitHub.github-vscode-theme
 codium --install-extension GrapeCity.gc-excelviewer
 codium --install-extension hashicorp.terraform
-codium --install-extension heaths.vscode-guid-1.7.0.vsix
 codium --install-extension hediet.vscode-drawio
-codium --install-extension henoc.svgeditor-2.9.0.vsix
 codium --install-extension hilleer.yaml-plus-json
 codium --install-extension jebbs.plantuml
 codium --install-extension jock.svg
 codium --install-extension johnpapa.vscode-peacock
 codium --install-extension mikestead.dotenv
-codium --install-extension ms-azuretools.vscode-bicep-0.23.1.vsix
 codium --install-extension redhat.vscode-yaml
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.vsix | Sort-Object $._FullName | ForEach-Object {
+    codium --install-extension $_.FullName
+}
