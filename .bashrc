f3ce1728dcfb4f9b65868189662f903be9314ddd
razvangoga <razvan.goga@gmail.com>
2024-09-08 19:09:43 +0200

    ubuntu setup

---
+++ .bashrc
@@ -0,0 +1,9 @@
+alias dc='docker compose'
+alias drdb='dotnet restore && dotnet build'
+alias gtcb='git checkout -b'
+alias ll='eza -al'
+alias ls='eza -a'
+alias md=mkdir
+alias rmd='rm -rf'
+
+eval "$(oh-my-posh init bash --config ~/OneDrive/Work/scripts-n-cheatsheets/ohmyposh.json)"
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,30 +1,52 @@
 #!/bin/sh
 
-#vscode 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
+#omakub
+wget -qO- https://omakub.org/install | bash
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+#disable omakub shortcuts - they conflict with Jetbrains tools
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"   
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"   
+
+#microsoft edge
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
+
+sudo apt update && sudo apt install microsoft-edge-stable
+
+#jetbrains toolbox
+sudo apt install libfuse2
+curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
 
 #azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# powershell core
-sudo apt-get update
-sudo apt-get install -y wget apt-transport-https software-properties-common
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo add-apt-repository universe
-sudo apt-get install -y powershell
+#tools
+snap install azuredatastudio
+snap install chromium
+snap install dotnet-sdk
+snap install drawio
+snap install gitkraken --classic
+snap install powershell
+snap install storage-explorer
 
+#onedrive sync
+sudo apt install onedrive #https://ubuntuhandbook.org/index.php/2024/02/install-onedrive-ubuntu/
+onedrive --synchronize --single-directory 'Projects'
+onedrive --synchronize --single-directory 'Work'
 
-#other tools
-sudo apt install git
+systemctl enable --user onedrive
+systemctl start --user onedrive
+systemctl status --user onedrive
 
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
+# configurations
+dotnet tool update -g linux-dev-certs
+dotnet linux-dev-certs install
