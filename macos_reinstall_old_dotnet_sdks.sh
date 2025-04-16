3ea4e93a87696e59346e8af6361050adcad6cb22
Razvan Goga <razvan.goga@gmail.com>
2025-04-16 10:29:26 +0200

    updated dotnet versions for macos

--- macos_reinstall_old_dotnet_sdks.sh
+++ macos_reinstall_old_dotnet_sdks.sh
@@ -1,8 +1,8 @@
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -v 6.0.425
-./dotnet-install.sh -v 8.0.404
-./dotnet-install.sh -v 9.0.101
+./dotnet-install.sh -v 6.0.428
+./dotnet-install.sh -v 8.0.408
+./dotnet-install.sh -v 9.0.203
 
 dotnet tool install -g dotnet-depends
 dotnet tool install -g dotnet-ef
@@ -11,7 +11,6 @@ dotnet tool install -g GitVersion.Tool
 dotnet tool install -g nbgv
 dotnet tool install -g tye2 --version 0.11.10
 
-sudo dotnet workload update
 dotnet dev-certs https --trust
 dotnet --info
 
